13c6d7cffe780cce2a10ca2e6b0385a808a1e744
Damian Borowski <48683790+Damian-Borowski@users.noreply.github.com>
2020-09-09 09:13:18 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-const btn = document.querySelector("button"); //nasluchiwanie elementu button
+const btn = document.querySelector("button"); 
 let number = 1; // 
 let activeNumber = 1;
-const addElement = () => {  //funkcja 
-    const box = document.createElement("div"); //tworzymy element div
-    box.textContent = number; //przypisujemy numer do elementu
+const addElement = () => { 
+    const box = document.createElement("div");
+    box.textContent = number;
     if(activeNumber == 5){
         activeNumber = 0;
         box.classList.add("bonus");
     }
-    document.body.appendChild(box); //dodajemy go w sekcji body
+    document.body.appendChild(box);
 
-    ++number; // zwiekszamy numer
+    ++number; // 
     activeNumber++;
 }; 
 
 
-btn.addEventListener("click", addElement) // nasluchiwanie na click
+btn.addEventListener("click", addElement) 
 
